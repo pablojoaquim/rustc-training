b153e06c08c2e019fd96ed3bfc681d513d7964a7
pablo.joaquim <pablo.joaquim@gmail.com>
2026-02-13 07:55:48 -0300

    Understanding match function, something similar to an IF statement.

--- hello-world/src/main.rs
+++ hello-world/src/main.rs
@@ -1,6 +1,7 @@
 
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 // We can create a project using cargo new.
 // We can build a project using cargo build.
@@ -13,8 +14,6 @@ fn main() {
     // we call the rand::thread_rng function that gives us the particular random number generator we’re going to use: 
     // one that is local to the current thread of execution and is seeded by the operating system
     let _secret_number = rand::thread_rng().gen_range(1..=100);
-
-    println!("Please input your guess.");
 
     // we’ll create a variable to store the user input
     // We use the let statement to create the variable
@@ -31,5 +30,25 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
+    // We create a variable named guess. But wait, doesn’t the program already 
+    // have a variable named guess? It does, but helpfully Rust allows us to shadow 
+    // the previous value of guess with a new one. Shadowing lets us reuse the guess 
+    // variable name rather than forcing us to create two unique variables, such as guess_str and guess
+    // We bind this new variable to the expression guess.trim().parse(). The guess in 
+    // the expression refers to the original guess variable that contained the input 
+    // as a string. The trim method on a String instance will eliminate any whitespace 
+    // at the beginning and end, which we must do before we can convert the string to 
+    // a u32, which can only contain numerical data.
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
     println!("You guessed: {guess}");
+
+    // A match expression is made up of arms. An arm consists of a pattern to match against, 
+    // and the code that should be run if the value given to match fits that arm’s pattern.
+    // Rust takes the value given to match and looks through each arm’s pattern in turn.
+    match guess.cmp(&_secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
