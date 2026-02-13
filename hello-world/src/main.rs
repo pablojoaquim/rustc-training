f020485633beee14cac3e044b71d7ba46a3d8672
pablo.joaquim <pablo.joaquim@gmail.com>
2026-02-13 07:38:48 -0300

    New concepts about variables and std::in has been added

--- hello-world/src/main.rs
+++ hello-world/src/main.rs
@@ -1,9 +1,28 @@
+
 // We can create a project using cargo new.
 // We can build a project using cargo build.
 // We can build and run a project in one step using cargo run.
 // We can build a project without producing a binary to check for errors using cargo check.
 // Instead of saving the result of the build in the same directory as our code, Cargo stores it in the target/debug directory.
-
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    // we’ll create a variable to store the user input
+    // We use the let statement to create the variable
+    // In Rust, variables are immutable by default, meaning 
+    // once we give the variable a value, the value won’t change
+    // To make a variable mutable, we add mut before the variable name
+    let mut guess = String::new();
+
+    // The & indicates that this argument is a reference, 
+    // which gives you a way to let multiple parts of your code access 
+    // one piece of data without needing to copy that data into memory 
+    // multiple times.
+    std::io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {guess}");
 }
